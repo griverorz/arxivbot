b57c35dafab17a243d1339e75d79d8bc1ef2819d
Gonzalo Rivero <griverorz@gmail.com>
2014-07-15 21:36:36 -0700

    Now mit classes

--- src/arxivbot.py
+++ src/arxivbot.py
@@ -4,109 +4,142 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
 import re
+import pandas as pd
 from datetime import datetime, timedelta
 import tweepy
 import sys
 import getopt
 import time
-import csv
-
-with(open('credentials.txt', 'rb')) as f:
-    creds = csv.reader(f, delimiter=',')
-    creds = {row[0]: row[1] for row in creds}
-
-auth = tweepy.OAuthHandler(creds['consumer_key'], creds['consumer_secret'])
-auth.set_access_token(creds['access_token_key'], creds['access_token_secret'])
-api = tweepy.API(auth)
+import os
 
 
-def get_papers(max_results):
-    baseurl = 'http://export.arxiv.org/api/query?search_query=cat:'
-    subjects = ('stat.AP', 'stat.CO', 'stat.ML', 'stat.ME', 'stat.TH')
-    results = '&start=0&max_results={0}'.format(max_results)
-    sorting = '&sortBy=submittedDate&sortOrder=descending'
-    url = baseurl + '+OR+'.join(subjects) + results + sorting
-    data = urllib2.urlopen(url).read()
+class papers(object):
 
-    fulldoc = BeautifulSoup(data)
-    papers = fulldoc.findAll('entry')
-    return papers
+    def __init__(self, fname, subjects, max_results, tdelta):
+        self.fname = fname
+        self.subjects = subjects
+        self.max_results = max_results
+        self.tdelta = tdelta
+        self.raw = None
+        self.data = None
 
 
-def clean_string(string):
-    string = unicode(string).encode('utf-8')
-    return re.sub('\n[ ]?', '', string)
+    def __getitem__(self, key):
+        return self.data[key]
 
 
-def parse_paper(papers):
-    titlink = [{'time': i.published.text,
-                'title': clean_string(i.title.text),
-                'link': i.id.text}
-               for i in papers]
-    return(titlink)
+    def get(self):
+        baseurl = 'http://export.arxiv.org/api/query?search_query=cat:'
+        results = '&start=0&max_results={0}'.format(self.max_results)
+        sorting = '&sortBy=submittedDate&sortOrder=descending'
+        url = baseurl + '+OR+'.join(self.subjects) + results + sorting
+        data = urllib2.urlopen(url).read()
+        
+        fulldoc = BeautifulSoup(data)
+        self.raw = fulldoc.findAll('entry')
 
 
-def valid_paper(paper, diff):
-    diff = timedelta(hours=diff)
-    curr = datetime.strptime(paper['time'], '%Y-%m-%dT%H:%M:%SZ')
-    return datetime.now() - curr < diff
+    def parse(self):
+
+        def _clean_string(string):
+            string = unicode(string).encode('utf-8')
+            return re.sub('\n[ ]?', '', string)
+
+        self.data = [{'time': i.published.text,
+                    'title': _clean_string(i.title.text),
+                    'link': i.id.text}
+                     for i in self.raw]
 
 
-def subset_papers(papers, diff):
-    return [i for i in papers if valid_paper(i, diff)]
+    def write(self):
+        pd.DataFrame(self.data).to_csv(self.fname, index=False, header=False)
 
 
-def shorten(string):
-    if len(string) > 121:
-        string = string[0:118] + '...'
-    return(string)
+    def validate(self, paper, timediff):
+        already_published = False
+        if os.path.isfile(self.fname):
+            if os.stat(self.fname).st_size is not 0:
+                tmp = pd.read_csv(self.fname, header=False)
+                already_published = (paper['link'] in str(tmp.ix[:, 0]))
+        timediff = timedelta(hours=timediff)
+        curr = datetime.strptime(paper['time'], '%Y-%m-%dT%H:%M:%SZ')
+        return (datetime.now() - curr < timediff) & (not already_published)
 
 
-def create_tweets(max_tweets, how_old):
-    data = get_papers(max_tweets)
-    papers = parse_paper(data)
-    papers = subset_papers(papers, how_old)
-    return ['{0} {1}'.format(shorten(i['title']), i['link']) for i in papers]
+    def output(self):
+        self.parse()
+        self.data = [i for i in self.data if self.validate(i, self.tdelta)]
+        self.write()
 
 
-def publish_tweets(api, tweets, sleeptime):
-    if tweets:
-        for i in tweets:
-            try:
-                api.update_status(i)
-                print 'Status: "' + i[0:50] + '" succesfully tweeted'
-            except tweepy.TweepError as e:
-                print e
-            time.sleep(sleeptime)
-    else:
-        print 'No tweet to publish'
+class tweet(object):
+
+    def __init__(self, credentials, data):
+        creds = pd.read_csv(credentials, header=None)
+        auth = tweepy.OAuthHandler(creds.ix[0, 1], creds.ix[1, 1])
+        auth.set_access_token(creds.ix[2, 1], creds.ix[3,1])
+        self.api = tweepy.API(auth)
+        self.data = data
+        self.tweets = None
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def create_tweets(self):
+        def _shorten(string):
+            if len(string) > 121:
+                string = string[0:118] + '...'
+            return(string)
+        self.tweets = ['{0} {1}'.format(_shorten(i['title']), i['link']) 
+                       for i in self.data]
+        
+
+    def publish(self, sleeptime):
+        if self.tweets:
+            for i in self.tweets:
+                try:
+                    self.api.update_status(i)
+                    print 'Status: "' + i[0:50] + '" succesfully tweeted'
+                except tweepy.TweepError as e:
+                    print e
+                    time.sleep(sleeptime)
+        else:
+            print 'No tweet to publish'
 
 
 def main(argv):
-    max_tweets = 50
-    how_old = 24
+    max_results = 50
+    tdelta = 50
+    subjects = ('stat.AP', 'stat.CO', 'stat.ML', 'stat.ME', 'stat.TH')
     publish = True
     try:
-        opts, args = getopt.getopt(argv, 'm:d:n',
-                                   ['maxtweets=', 'delta=', 'nopub='])
+        opts, args = getopt.getopt(argv, 'c:f:m:t:n')
     except getopt.GetoptError:
-        print 'test.py -m <maxtweets> -d <deltatime> -n <nopublish>'
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print 'test.py -m <maxtweets> -d <delta>'
+            print 'arxivbot.py -c <str> -f <str> -m <int> -d <int> -n'
             sys.exit()
-        elif opt in ("-m", "--maxtweets"):
-            max_tweets = int(arg)
-        elif opt in ("-d", "--delta"):
-            how_old = int(arg)
-        elif opt in ("-n", "--nopublish"):
+        if opt == '-f':
+            filename = str(arg)
+        if opt == '-c':
+            credentials = str(arg)
+        if opt == '-m':
+            max_results = int(arg)
+        if opt == '-t':
+            tdelta = int(arg)
+        if opt == '-n':
             publish = False
-    tweets = create_tweets(max_tweets, how_old)
+    data = papers(filename, subjects, max_results, tdelta)
+    data.get()
+    data.output()
+
+    tw = tweet(credentials, data)
+    tw.create_tweets()
     if publish:
-        publish_tweets(api, tweets, 60*10)
+        tw.publish(60*10)
     else:
-        for i in tweets:
+        for i in tw.tweets:
             print i + '\n'
 
 
